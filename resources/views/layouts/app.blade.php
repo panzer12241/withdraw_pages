8b9b622df7fbf518f0f4ad02c590e1a0c8c577d7
Your Name <you@example.com>
2026-02-27 20:57:39 +0700

    remove bot

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="noindex, nofollow, noarchive, nosnippet">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'MyApp') }} — @yield('title', 'Home')</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
